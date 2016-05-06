ee584d920759b7b349f94829a9902624be78e5dd
Eric Czech <eric.allen.czech@gmail.com>
2016-05-05 23:30:37 -0400

    Feature analysis for cosmic

--- src/R/main/data_model/feature_analysis.R
+++ src/R/main/data_model/feature_analysis.R
@@ -50,50 +50,115 @@ if (sum(!is.na(d.predict$tissue)) != nrow(d.subtype))
 top.feats <- GetTopFeatures(RESULT_CACHE, GetOptimalFeatCt())
 top.feats <- top.feats %>% filter(selected.in.uk )
 
-# d.prep %>%
-#   filter(origin == 'LUNG' & tissue == 'LUNG_SMALL_CELL_CARCINOMA') %>% 
-#   select(one_of(c(top.feats$feature, 'tumor_id')), -origin) %>% 
-#   melt(id.vars=c('tumor_id')) %>% 
-#   ggplot(aes(x=variable, y=tumor_id, fill=value)) + geom_tile()
+d.prep[,c(top.feats$feature, 'tumor_id')] %>%
+  filter(origin == 'LUNG' & tissue == 'LUNG_SMALL_CELL_CARCINOMA') %>%
+  select(-origin) %>%
+  melt(id.vars=c('tumor_id')) %>%
+  ggplot(aes(x=variable, y=tumor_id, fill=value)) + geom_tile()
 
-feat.map <- d.predict %>%
-  select(one_of(top.feats$feature), tumor_id, origin, tissue, prob) %>% 
-  mutate(pred_type=ifelse(prob > .8, 'HI', 'LO')) %>% select(-prob) %>%
-  filter(origin == 'LUNG') %>%
-  filter(tissue == 'LUNG_SMALL_CELL_CARCINOMA') %>% 
-  select(-origin, -tissue) %>%
-  melt(id.vars=c('tumor_id', 'pred_type')) 
 
-# feat.map %>%
-#   ggplot(aes(x=variable, y=tumor_id, fill=value)) + geom_tile() +
-#   facet_wrap(~pred_type, scales='free_y', ncol=1)
-    
-
-prep.features <- function(d, pred.type){
-  r <- d %>% filter(pred_type == pred.type)
-  r <- reshape2::dcast(r, tumor_id ~ variable, value.var='value') 
-  rownames(r) <- as.character(r$tumor_id)
-  r <- r %>% select(-tumor_id)
-  r[order(predict(pca, r)[,'PC1']),]
+get.feat.map <- function(d){
+  d[,c(top.feats$feature, 'tumor_id', 'origin', 'tissue', 'prob')] %>%
+    mutate(pred_type=ifelse(prob <= .8, ifelse(prob < 0, 'TRAIN', 'LO'), 'HI')) %>% select(-prob) %>%
+    filter(origin == 'LUNG') %>%
+    filter(tissue == 'LUNG_SMALL_CELL_CARCINOMA') %>% 
+    select(-origin, -tissue) 
 }
 
+reorder.pca <- function(d){
+  d.order <- d %>% select(-tumor_id, -pred_type)
+  d[order(predict(prcomp(d.order), d.order)[,'PC1']),]
+}
 
-d.hi <- prep.features(feat.map, 'HI')
-d.lo <- prep.features(feat.map, 'LO')
-high <- as.matrix(d.hi)
-low <- as.matrix(d.lo)
-subplot(
-  plot_ly(z = high, x = colnames(d.hi), y = rownames(d.hi), type = "heatmap", colors='YlOrRd', showscale = F, zmin=-3, zmax=6, width=1200),
-  plot_ly(z = low, x = colnames(d.lo), y = rownames(d.lo), type = "heatmap", colors='YlOrRd', showscale = T, zmin=-3, zmax=6, colorbar=list(title='Feature Value'), width=1200),
-  margin = 0.05, 
-  nrows=2
-) %>% layout(
-  showlegend = F, 
-  title='Feature Values for Hi/Lo Predictions', 
-  xaxis=list(title='Cell Lines w/ High Predicted Sensitivity'),
-  yaxis=list(title=''),
-  xaxis2=list(title='Cell Lines w/ Low Predicted Sensitivity'),
-  yaxis2=list(title='')
+feat.map <- rbind(get.feat.map(d.predict), get.feat.map(d.prep %>% mutate(prob=-1)))
+
+
+d.hi <- feat.map %>% filter(pred_type=='HI') %>% reorder.pca
+p.hi <- plot_ly(
+  x=d.hi %>% select(-pred_type, -tumor_id) %>% names,
+  y=d.hi$tumor_id, 
+  z=d.hi %>% select(-pred_type, -tumor_id) %>% as.matrix,
+  type='heatmap', showscale = T, zmin=-3, zmax=6, 
+  colorbar=list(title='Feature Value'), width=1200,
+  colors='YlOrRd'
+)
+
+d.lo <- feat.map %>% filter(pred_type=='LO') %>% reorder.pca
+p.lo <- plot_ly(
+  x=d.lo %>% select(-pred_type, -tumor_id) %>% names,
+  y=d.lo$tumor_id, 
+  z=d.lo %>% select(-pred_type, -tumor_id) %>% as.matrix, 
+  type='heatmap', showscale = F, zmin=-3, zmax=6
+)
+
+d.tr <- feat.map %>% filter(pred_type=='TRAIN') %>% reorder.pca
+d.tr.res <- d.prep[,c('tumor_id', 'response')] %>%
+  mutate(response = as.character(DichotomizeOutcome(response, RESPONSE_THRESH)))
+d.tr <- d.tr %>% left_join(d.tr.res, by='tumor_id') %>%
+  mutate(tumor_id=sprintf('%s (%s)', tumor_id, response)) %>% select(-response)
+
+p.tr <- plot_ly(
+  x=d.tr %>% select(-pred_type, -tumor_id) %>% names,
+  y=d.tr$tumor_id, 
+  z=d.tr %>% select(-pred_type, -tumor_id) %>% as.matrix, 
+  type='heatmap', showscale = F, zmin=-3, zmax=6
 )
 
 
+subplot(p.hi, p.lo, p.tr, margin = 0.05, nrows=3) %>% layout(
+  showlegend = F, 
+  title='Feature Values for Training and Predicted Cell Lines (by Hi/Lo prediction)', 
+  xaxis=list(title='Cell Lines w/ High Predicted Sensitivity'),
+  yaxis=list(title=''),
+  xaxis2=list(title='Cell Lines w/ Low Predicted Sensitivity'),
+  yaxis2=list(title=''),
+  xaxis3=list(title='Training Cell Lines'),
+  yaxis3=list(title='')
+)
+
+
+
+# Defunct Now:
+
+# 
+# # feat.map %>%
+# #   ggplot(aes(x=variable, y=tumor_id, fill=value)) + geom_tile() +
+# #   facet_wrap(~pred_type, scales='free_y', ncol=1)
+#     
+# 
+# prep.features <- function(d, pred.type){
+#   r <- d %>% filter(pred_type == pred.type)
+#   r <- reshape2::dcast(r, tumor_id ~ variable, value.var='value') 
+#   rownames(r) <- as.character(r$tumor_id)
+#   r <- r %>% select(-tumor_id)
+#   r[order(predict(prcomp(r), r)[,'PC1']),]
+# }
+# 
+# ### Single Prob Type (training data)
+# d.hi <- prep.features(feat.map, 'HI')
+# plot_ly(
+#   z = unname(d.hi), x = colnames(d.hi), y = rownames(d.hi), 
+#   type = "heatmap", #colors='YlOrRd', 
+#   showscale = F, zmin=-3, zmax=6, width=1200
+# )
+# 
+# ### Binary Prob Type (prediction data)
+# d.hi <- prep.features(feat.map, 'HI')
+# d.lo <- prep.features(feat.map, 'LO')
+# high <- as.matrix(d.hi)
+# low <- as.matrix(d.lo)
+# subplot(
+#   plot_ly(z = high, x = colnames(d.hi), y = rownames(d.hi), type = "heatmap", colors='YlOrRd', showscale = F, zmin=-3, zmax=6, width=1200),
+#   plot_ly(z = low, x = colnames(d.lo), y = rownames(d.lo), type = "heatmap", colors='YlOrRd', showscale = T, zmin=-3, zmax=6, colorbar=list(title='Feature Value'), width=1200),
+#   margin = 0.05, 
+#   nrows=2
+# ) %>% layout(
+#   showlegend = F, 
+#   title='Feature Values for Hi/Lo Predictions', 
+#   xaxis=list(title='Cell Lines w/ High Predicted Sensitivity'),
+#   yaxis=list(title=''),
+#   xaxis2=list(title='Cell Lines w/ Low Predicted Sensitivity'),
+#   yaxis2=list(title='')
+# )
+# 
+# 
